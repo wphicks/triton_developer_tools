d1da53db17d2d8dc9b02b08a5b624a70e13f269a
divyegala <divyegala@gmail.com>
2022-10-14 10:44:26 -0700

    working through docker and cmake build

--- examples/backend_linear/cmake/thirdparty/get_dev_tools.cmake
+++ examples/backend_linear/cmake/thirdparty/get_dev_tools.cmake
@@ -14,33 +14,33 @@
 # limitations under the License.
 #=============================================================================
 
-function(find_and_configure_rapids_triton)
+function(find_and_configure_dev_tools)
 
     set(oneValueArgs VERSION FORK PINNED_TAG)
     cmake_parse_arguments(PKG "${options}" "${oneValueArgs}"
                           "${multiValueArgs}" ${ARGN} )
 
-    rapids_cpm_find(rapids_triton ${PKG_VERSION}
-      GLOBAL_TARGETS      rapids_triton::rapids_triton
-      BUILD_EXPORT_SET    rapids_triton_linear-exports
-      INSTALL_EXPORT_SET  rapids_triton_linear-exports
+    rapids_cpm_find(triton_developer_tools ${PKG_VERSION}
+      GLOBAL_TARGETS      developer_tools::developer_tools
+      BUILD_EXPORT_SET    developer_tools_linear-exports
+      INSTALL_EXPORT_SET  developer_tools_linear-exports
         CPM_ARGS
-            GIT_REPOSITORY https://github.com/${PKG_FORK}/rapids-triton.git
+            GIT_REPOSITORY https://github.com/${PKG_FORK}/triton_developer_tools.git
             GIT_TAG        ${PKG_PINNED_TAG}
-            SOURCE_SUBDIR  cpp
+            SOURCE_SUBDIR  backend/cpp
             OPTIONS
               "BUILD_TESTS OFF"
               "BUILD_EXAMPLE OFF"
     )
 
-  message(VERBOSE "RAPIDS_TRITON_LINEAR: Using RAPIDS-Triton located in ${rapids_triton_SOURCE_DIR}")
+  message(STATUS "dev_tools_LINEAR: Using developer_tools located in ${developer_tools_SOURCE_DIR}")
 
 endfunction()
 
 # Change pinned tag here to test a commit in CI
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
-find_and_configure_rapids_triton(VERSION    21.10
-                        FORK       rapidsai
-                        PINNED_TAG branch-21.10
+find_and_configure_dev_tools(VERSION    22.10
+                        FORK       wphicks
+                        PINNED_TAG fea-backend
                         )
